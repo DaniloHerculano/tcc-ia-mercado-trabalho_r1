683d14a5b40dceb36fa2b77eccd5130237dc7bf3
Danilo Valentim Herculano <herculano@MacBook-Air-de-Danilo.local>
2026-05-21 21:49:42 -0300

    debug import

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,122 +1,19 @@
 import streamlit as st
 
-from carregar_dados import (
-    criar_base_final
-)
-
-from views.dashboard_home import mostrar_dashboard
-from views.impacto_brasil import mostrar_impacto_brasil
-from views.evolucao_temporal import mostrar_evolucao_temporal
-from views.escolaridade import mostrar_escolaridade
-from views.renda import mostrar_renda
-from views.setores import mostrar_setores
-from views.ranking import mostrar_ranking
-from views.similaridade import mostrar_similaridade
-from views.pesquisa import mostrar_pesquisa
-from views.sobre import mostrar_sobre
-
-# ==========================================
-# CONFIG
-# ==========================================
-
 st.set_page_config(
-    page_title="IA e Mercado de Trabalho",
+    page_title="TESTE",
     layout="wide"
 )
 
-# ==========================================
-# CACHE
-# ==========================================
+st.title("TESTE INICIAL")
 
-@st.cache_data
-def carregar():
+try:
 
-    df = criar_base_final()
+    import carregar_dados
 
-    return df
+    st.success("carregar_dados importado!")
 
-# ==========================================
-# LOADING
-# ==========================================
+except Exception as e:
 
-with st.spinner("Carregando dados..."):
-
-    df = carregar()
-
-# ==========================================
-# MENU
-# ==========================================
-
-st.sidebar.title("📌 Navegação")
-
-pagina = st.sidebar.radio(
-    "Selecione:",
-    [
-        "📊 Dashboard",
-        "🇧🇷 Impacto no Brasil",
-        "📈 Evolução Temporal",
-        "🎓 Escolaridade x IA",
-        "💰 Renda x IA",
-        "🏭 Setores x IA",
-        "🏆 Ranking",
-        "🧠 Similaridade",
-        "🔎 Pesquisar Ocupação",
-        "ℹ️ Sobre"
-    ]
-)
-
-# ==========================================
-# ROTAS
-# ==========================================
-
-if pagina == "📊 Dashboard":
-
-    mostrar_dashboard(df)
-
-elif pagina == "🇧🇷 Impacto no Brasil":
-
-    mostrar_impacto_brasil(
-        df,
-        df
-    )
-
-elif pagina == "📈 Evolução Temporal":
-
-    mostrar_evolucao_temporal(
-        df,
-        df
-    )
-
-elif pagina == "🎓 Escolaridade x IA":
-
-    mostrar_escolaridade(
-        df,
-        df
-    )
-
-elif pagina == "💰 Renda x IA":
-
-    mostrar_renda(
-        df,
-        df
-    )
-
-elif pagina == "🏭 Setores x IA":
-
-    mostrar_setores(df)
-
-elif pagina == "🏆 Ranking":
-
-    mostrar_ranking(df)
-
-elif pagina == "🧠 Similaridade":
-
-    mostrar_similaridade(df)
-
-elif pagina == "🔎 Pesquisar Ocupação":
-
-    mostrar_pesquisa(df)
-
-elif pagina == "ℹ️ Sobre":
-
-    mostrar_sobre()
+    st.error("ERRO AO IMPORTAR carregar_dados")
+    st.exception(e)
