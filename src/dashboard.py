b23ea066f7668a101f5aaed2d801bb19a3605fe1
Danilo Valentim Herculano <herculano@MacBook-Air-de-Danilo.local>
2026-05-21 22:24:36 -0300

    fix parquet engine

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import pandas as pd
 
 from carregar_dados import (
     carregar_pnad,
-    carregar_cbo
+    carregar_cbo,
+    limpar_cbo,
+    limpar_pnad,
+    cruzar_bases
 )
 
 st.set_page_config(
@@ -11,40 +13,61 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("DEBUG FINAL")
+st.title("DEBUG FINAL 2")
 
 # ==========================================
-# TESTE PNAD
+# PNAD
 # ==========================================
 
-try:
+st.write("1 - Carregando PNAD")
 
-    st.write("TESTE PARQUET")
+df_pnad = carregar_pnad()
 
-    df_pnad = carregar_pnad()
-
-    st.success("PARQUET OK")
-
-    st.write(df_pnad.head())
-
-except Exception as e:
-
-    st.error(f"ERRO PARQUET: {e}")
+st.success("PNAD OK")
 
 # ==========================================
-# TESTE CBO
+# CBO
 # ==========================================
 
-try:
+st.write("2 - Carregando CBO")
 
-    st.write("TESTE CBO")
+df_cbo = carregar_cbo()
 
-    df_cbo = carregar_cbo()
+st.success("CBO OK")
 
-    st.success("CBO OK")
+# ==========================================
+# LIMPEZA CBO
+# ==========================================
 
-    st.write(df_cbo.head())
+st.write("3 - Limpando CBO")
 
-except Exception as e:
+df_cbo = limpar_cbo(df_cbo)
 
-    st.error(f"ERRO CBO: {e}")
+st.success("LIMPEZA CBO OK")
+
+# ==========================================
+# LIMPEZA PNAD
+# ==========================================
+
+st.write("4 - Limpando PNAD")
+
+df_pnad = limpar_pnad(df_pnad)
+
+st.success("LIMPEZA PNAD OK")
+
+# ==========================================
+# MERGE
+# ==========================================
+
+st.write("5 - Fazendo merge")
+
+df_final = cruzar_bases(
+    df_cbo,
+    df_pnad
+)
+
+st.success("MERGE OK")
+
+st.write(df_final.head())
+
+st.write(df_final.columns.tolist())
