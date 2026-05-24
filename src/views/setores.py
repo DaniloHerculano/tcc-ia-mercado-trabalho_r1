f86e614cb6072a8d8264fcbe47105ce7e36aad91
Danilo Herculano <101299841+DaniloHerculano@users.noreply.github.com>
2026-05-24 12:34:33 -0300

    Update setores.py

--- src/views/setores.py
+++ src/views/setores.py
@@ -10,7 +10,7 @@ import json
 
 def mostrar_setores(df):
 
-    st.title("🏭 Mapa de Exposição a IA)
+    st.title("🏭 Mapa de Exposição a IA")
 
     st.markdown("""
     Visualização geográfica da exposição média à Inteligência Artificial
